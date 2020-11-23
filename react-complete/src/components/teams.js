e6de1abcf55b27b3b898883cce2639c0e6e9439d
Ko Hyunmin <junseol86@gmail.com>
2020-11-24 01:30:42 +0900

    add contents part

--- react-complete/src/components/teams.js
+++ react-complete/src/components/teams.js
@@ -1,4 +1,5 @@
 import './components.css';
+import { useState } from 'react';
 import { useQuery, gql } from '@apollo/client';
 
 const GET_TEAMS = gql`
@@ -18,23 +19,30 @@ const GET_TEAMS = gql`
 
 function Teams() {
 
-  function AsideItems () {
-    const { loading, error, data } = useQuery(GET_TEAMS);
+
+
+  const [contentId, setContentId] = useState('1')
+
+  function callSetContentId(id) {
+    setContentId(id)
+  }
+
+
+  function AsideItems (data) {
     const roleIcons = {
       developer: '💻',
       designer: '🎨',
       planner: '📝'
     }
 
-    if (loading) return <p className="loading">Loading...</p>
-    if (error) return <p className="error">Error :(</p>
-
     return (
       <ul>
         {data.teams.map(({id, manager, members}) => {
           return (
             <li key={id}>
-              <span className="teamItemTitle">Team {id} : {manager}'s</span>
+              <span className="teamItemTitle" onClick={() => {callSetContentId(id)}}>
+                Team {id} : {manager}'s
+              </span>
               <ul className="teamMembers">
                 {members.map(({id, first_name, last_name, role, tools}) => {
                   return (
@@ -51,17 +59,58 @@ function Teams() {
     )
   }
 
-  function MainContents () {
-    return (<div></div>);
+  function MainContents (data) {
+    return (
+      <div>
+        {data.teams.map(({id, manager}) => {
+          return (
+            <div className="inputContainer">
+              <table>
+                <tr>
+                  <td>Manager</td>
+                  <td><input type="text"/></td>
+                </tr>
+                <tr>
+                  <td>Office</td>
+                  <td><input type="text"/></td>
+                </tr>
+                <tr>
+                  <td>Extension Number</td>
+                  <td><input type="text"/></td>
+                </tr>
+                <tr>
+                  <td>Mascot</td>
+                  <td><input type="text"/></td>
+                </tr>
+                <tr>
+                  <td>Cleaning Duty</td>
+                  <td><input type="text"/></td>
+                </tr>
+                <tr>
+                  <td>Project</td>
+                  <td><input type="text"/></td>
+                </tr>
+              </table>
+
+            </div>
+          )
+        })}
+      </div>
+    )
   }
+
+  const { loading, error, data } = useQuery(GET_TEAMS);
+
+  if (loading) return <p className="loading">Loading...</p>
+  if (error) return <p className="error">Error :(</p>
 
   return (
     <div id="teams" className="component">
         <aside>
-            {AsideItems()}
+            {AsideItems(data)}
         </aside>
         <section className="contents">
-            {MainContents()}
+            {MainContents(data)}
         </section>
     </div>
   )
