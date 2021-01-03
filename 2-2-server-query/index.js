b4fa7d7ed0b146b342d8ad39eed2f054068752c2
Ko Hyunmin <junseol86@gmail.com>
2021-01-04 00:02:44 +0900

    add to 3-3

--- 2-2-server-query/index.js
+++ 2-2-server-query/index.js
@@ -2,18 +2,18 @@ const database = require('./database')
 const { ApolloServer, gql } = require('apollo-server')
 ​
 const typeDefs = gql`
-type Query {
-  teams: [Team]
-}
-type Team {
-  id: Int
-  manager: String
-  office: String
-  extension_number: String
-  mascot: String
-  cleaning_duty: String
-  project: String
-}
+    type Query {
+        teams: [Team]
+    }
+    type Team {
+        id: Int
+        manager: String
+        office: String
+        extension_number: String
+        mascot: String
+        cleaning_duty: String
+        project: String
+    }
 `
 const resolvers = {
   Query: {
